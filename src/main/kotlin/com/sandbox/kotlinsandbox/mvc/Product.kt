1f3987368ff48c2ea17e9f6fa2667b62013c9940
bluecheat <itsinil@gmail.com>
2024-02-27 18:44:34 +0900

    init project

---
+++ src/main/kotlin/com/sandbox/kotlinsandbox/mvc/Product.kt
@@ -0,0 +1,3 @@
+package com.sandbox.kotlinsandbox.mvc
+
+class Product
---
+++ src/test/kotlin/com/sandbox/kotlinsandbox/KotlinSandboxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sandbox.kotlinsandbox
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSandboxApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
